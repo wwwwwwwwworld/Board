9d14c2aa23721cd5db81b027e0880e750a2a3a52
satang1245 <satang1245@gmail.com>
2024-11-14 17:19:18 +0900

    수정

---
+++ src/main/java/com/example/boardproj/entity/Memo.java
@@ -0,0 +1,27 @@
+package com.example.boardproj.entity;
+
+import com.example.boardproj.entity.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Memo extends BaseEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+}
